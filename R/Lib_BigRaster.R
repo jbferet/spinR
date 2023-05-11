73ff7ab73b22920b0b014dd9aabcdd063bbe167f
Jean-Baptiste Feret <jb.feret@teledetection.fr>
2023-05-11 15:40:37 +0200

    added gc() to big raster functions

--- R/Lib_BigRaster.R
+++ R/Lib_BigRaster.R
@@ -1,3 +1,116 @@
+#' this function aims at computing spectral indices from big raster data
+#' it computes the spectral indices based on clsest bands with Sentinel-2
+#' and assumes that the bands of the S2 data follow this order
+#' wavelength	= {496.6, 560.0, 664.5, 703.9, 740.2, 782.5, 835.1, 864.8, 1613.7, 2202.4}
+#' Full description of the indices can be found here:
+#' https://www.sentinel-hub.com/eotaxonomy/indices
+#'
+#' @param raster_path character. path for the big raster
+#' @param PathOut character. path for output directory
+#' @param MaskRaster character. path for mask file corresponding to raster_path
+#' @param SensorBands numeric. wavelength in nanometers of the spectral bands of raster_path
+#' @param Sel_Indices  list. list of spectral indices to be computed
+#' @param ReflFactor numeric. multiplying factor used to write reflectance in image (==10000 for S2)
+#' @param Offset numeric. offset (when Refl between 0 and 1) to be applied on reflectance. Useful to avoid zero values
+#' @param S2Bands numeric. wavelength of the spectral bands corresponding to S2 (default = S2A)
+#'
+#' @return list. list of rasters corresponding to spectral indices
+#' @importFrom methods is
+#' @importFrom progress progress_bar
+#' @importFrom raster raster brick blockSize readStart readStop getValues writeStart writeStop writeValues
+#' @export
+
+compute_S2SI_BigRaster <- function(raster_path, PathOut, MaskRaster = FALSE,
+                                   SensorBands, Sel_Indices = 'ALL',
+                                   ReflFactor = 1, Offset = 0,
+                                   S2Bands = data.frame('B2'=492.7, 'B3'=559.8, 'B4'=664.6,
+                                                        'B5'=704.1, 'B6'=740.5, 'B7' = 782.8,
+                                                        'B8' = 832.8, 'B8A' = 864.7,
+                                                        'B11' = 1613.7, 'B12' = 2202.4)){
+  SpectralIndices <- list()
+  Sen2S2 <- get_closest_bands(SensorBands,S2Bands)
+  # read by chunk to avoid memory problem
+  blk <- blockSize(brick(raster_path))
+  # reflectance file
+  r_in <- readStart(brick(raster_path))
+  # mask file
+  r_inmask <- FALSE
+  if (!MaskRaster==FALSE){
+    if (file.exists(MaskRaster)){
+      r_inmask <- readStart(raster(MaskRaster))
+    } else if (!file.exists(MaskRaster)){
+      message('WARNING: Mask file does not exist:')
+      print(MaskRaster)
+      message('Processing all image')
+    }
+  }
+  listIndices <- list('ARI1','ARI2','ARVI','BAI','BAIS2','CCCI','CHL_RE','CRI1','CRI2','EVI','EVI2',
+                      'GRVI1','GNDVI','IRECI','LAI_SAVI','MCARI','mNDVI705','MSAVI2',
+                      'MSI','mSR705','MTCI','nBR_RAW','NDI_45','NDII','NDSI','NDVI','NDVI_G',
+                      'NDVI705','NDWI1','NDWI2','PSRI','PSRI_NIR','RE_NDVI','RE_NDWI','S2REP',
+                      'SAVI','SIPI','SR','CR_SWIR','CR_RE')
+  if (Sel_Indices[1]=='ALL'){
+    Sel_Indices <- listIndices
+  }
+  # initiate progress bar
+  pgbarlength <- blk$n
+  pb <- progress_bar$new(
+    format = "Computing SI [:bar] :percent in :elapsedfull , estimated time remaining :eta",
+    total = pgbarlength, clear = FALSE, width= 100)
+  # output files
+  SIpath <- r_out <- list()
+  for (SI in Sel_Indices){
+    SIpath[[SI]] <- file.path(PathOut,paste(basename(raster_path),SI,sep = '_'))
+    r_out[[SI]] <- writeStart(raster(raster_path), filename = SIpath[[SI]],format = "ENVI", overwrite = TRUE)
+  }
+  SIpath <- lapply(SIpath,FUN = paste, '.envi',sep = '')
+  # loop over blocks
+  for (i in seq_along(blk$row)) {
+    # read values for block
+    # format is a matrix with rows the cells values and columns the layers
+    BlockVal <- getValues(r_in, row = blk$row[i], nrows = blk$nrows[i])
+    FullLength <- dim(BlockVal)[1]
+
+    if (typeof(r_inmask)=='logical'){
+      # automatically filter pixels corresponding to negative values
+      SelectPixels <- which(BlockVal[,1]>0)
+      BlockVal <- BlockVal[SelectPixels,Sen2S2]
+    } else if (typeof(r_inmask)=='S4'){
+      MaskVal <- getValues(r_inmask, row = blk$row[i], nrows = blk$nrows[i])
+      SelectPixels <- which(MaskVal ==1)
+      BlockVal <- BlockVal[SelectPixels,Sen2S2]
+    }
+    names(BlockVal) <- names(Sen2S2)
+
+    for (SI in Sel_Indices){
+      SIval <- NA*vector(length = FullLength)
+      if (length(SelectPixels)>0){
+        BlockVal <- BlockVal/ReflFactor
+        # compute spectral index
+        SIvaltmp <- compute_S2SI_from_Sensor(Refl = BlockVal,
+                                             SensorBands = S2Bands,
+                                             Sel_Indices = SI)
+        SIval[SelectPixels] <- c(SIvaltmp$SpectralIndices[[SI]])
+      }
+      r_out[[SI]] <- writeValues(r_out[[SI]], SIval, blk$row[i],format = "ENVI", overwrite = TRUE)
+    }
+    pb$tick()
+  }
+  # close files
+  r_in <- readStop(r_in)
+  if (typeof(r_inmask)=='S4'){
+    r_inmask <- readStop(r_inmask)
+  }
+  for (SI in Sel_Indices){
+    r_out[[SI]] <- writeStop(r_out[[SI]])
+    # write biophysical variable name in headers
+    HDR <- read_ENVI_header(get_HDR_name(SIpath[[SI]]))
+    HDR$`band names` <- paste('{',SI,'}',sep = '')
+    write_ENVI_header(HDR, get_HDR_name(SIpath[[SI]]))
+  }
+  gc()
+  return(SIpath)
+}
 
 #' this function aims at stacking individual rasters into a unique raster file
 #' when the raster size is important.
@@ -71,9 +184,11 @@ stack_BigRaster <- function(list_rasters, stack_file, MaskRaster = FALSE,
     r_inmask <- readStop(r_inmask)
   }
   r_out <- writeStop(r_out)
+  stack_file <- paste(stack_file, '.envi',sep = '')
   HDR <- read_ENVI_header(get_HDR_name(stack_file))
   HDR$`band names` <- paste('',names_rasters,'',sep = '')
   write_ENVI_header(HDR, get_HDR_name(stack_file))
+  gc()
   return(stack_file)
 }
 
@@ -212,116 +327,3 @@ split_line <- function(x, separator, trim.blank = TRUE) {
   return(value)
 }
 
-
-#' this function aims at computing spectral indices from big raster data
-#' it computes the spectral indices based on clsest bands with Sentinel-2
-#' and assumes that the bands of the S2 data follow this order
-#' wavelength	= {496.6, 560.0, 664.5, 703.9, 740.2, 782.5, 835.1, 864.8, 1613.7, 2202.4}
-#' Full description of the indices can be found here:
-#' https://www.sentinel-hub.com/eotaxonomy/indices
-#'
-#' @param raster_path character. path for the big raster
-#' @param PathOut character. path for output directory
-#' @param MaskRaster character. path for mask file corresponding to raster_path
-#' @param SensorBands numeric. wavelength in nanometers of the spectral bands of raster_path
-#' @param Sel_Indices  list. list of spectral indices to be computed
-#' @param ReflFactor numeric. multiplying factor used to write reflectance in image (==10000 for S2)
-#' @param Offset numeric. offset (when Refl between 0 and 1) to be applied on reflectance. Useful to avoid zero values
-#' @param S2Bands numeric. wavelength of the spectral bands corresponding to S2 (default = S2A)
-#'
-#' @return list. list of rasters corresponding to spectral indices
-#' @importFrom methods is
-#' @importFrom progress progress_bar
-#' @importFrom raster raster brick blockSize readStart readStop getValues writeStart writeStop writeValues
-#' @export
-
-compute_S2SI_BigRaster <- function(raster_path, PathOut, MaskRaster = FALSE,
-                                   SensorBands, Sel_Indices = 'ALL',
-                                   ReflFactor = 1, Offset = 0,
-                                   S2Bands = data.frame('B2'=492.7, 'B3'=559.8, 'B4'=664.6,
-                                                        'B5'=704.1, 'B6'=740.5, 'B7' = 782.8,
-                                                        'B8' = 832.8, 'B8A' = 864.7,
-                                                        'B11' = 1613.7, 'B12' = 2202.4)){
-  SpectralIndices <- list()
-  Sen2S2 <- get_closest_bands(SensorBands,S2Bands)
-  # read by chunk to avoid memory problem
-  blk <- blockSize(brick(raster_path))
-  # reflectance file
-  r_in <- readStart(brick(raster_path))
-  # mask file
-  r_inmask <- FALSE
-  if (!MaskRaster==FALSE){
-    if (file.exists(MaskRaster)){
-      r_inmask <- readStart(raster(MaskRaster))
-    } else if (!file.exists(MaskRaster)){
-      message('WARNING: Mask file does not exist:')
-      print(MaskRaster)
-      message('Processing all image')
-    }
-  }
-  listIndices <- list('ARI1','ARI2','ARVI','BAI','BAIS2','CCCI','CHL_RE','CRI1','CRI2','EVI','EVI2',
-                      'GRVI1','GNDVI','IRECI','LAI_SAVI','MCARI','mNDVI705','MSAVI2',
-                      'MSI','mSR705','MTCI','nBR_RAW','NDI_45','NDII','NDSI','NDVI','NDVI_G',
-                      'NDVI705','NDWI1','NDWI2','PSRI','PSRI_NIR','RE_NDVI','RE_NDWI','S2REP',
-                      'SAVI','SIPI','SR','CR_SWIR','CR_RE')
-  if (Sel_Indices[1]=='ALL'){
-    Sel_Indices <- listIndices
-  }
-  # initiate progress bar
-  pgbarlength <- blk$n
-  pb <- progress_bar$new(
-    format = "Computing SI [:bar] :percent in :elapsedfull , estimated time remaining :eta",
-    total = pgbarlength, clear = FALSE, width= 100)
-  # output files
-  SIpath <- r_out <- list()
-  for (SI in Sel_Indices){
-    SIpath[[SI]] <- file.path(PathOut,paste(basename(raster_path),SI,sep = '_'))
-    r_out[[SI]] <- writeStart(raster(raster_path), filename = SIpath[[SI]],format = "ENVI", overwrite = TRUE)
-  }
-  SIpath <- lapply(SIpath,FUN = paste, '.envi',sep = '')
-  # loop over blocks
-  for (i in seq_along(blk$row)) {
-    # read values for block
-    # format is a matrix with rows the cells values and columns the layers
-    BlockVal <- getValues(r_in, row = blk$row[i], nrows = blk$nrows[i])
-    FullLength <- dim(BlockVal)[1]
-
-    if (typeof(r_inmask)=='logical'){
-      # automatically filter pixels corresponding to negative values
-      SelectPixels <- which(BlockVal[,1]>0)
-      BlockVal <- BlockVal[SelectPixels,Sen2S2]
-    } else if (typeof(r_inmask)=='S4'){
-      MaskVal <- getValues(r_inmask, row = blk$row[i], nrows = blk$nrows[i])
-      SelectPixels <- which(MaskVal ==1)
-      BlockVal <- BlockVal[SelectPixels,Sen2S2]
-    }
-    names(BlockVal) <- names(Sen2S2)
-
-    for (SI in Sel_Indices){
-      SIval <- NA*vector(length = FullLength)
-      if (length(SelectPixels)>0){
-        BlockVal <- BlockVal/ReflFactor
-        # compute spectral index
-        SIvaltmp <- compute_S2SI_from_Sensor(Refl = BlockVal,
-                                             SensorBands = S2Bands,
-                                             Sel_Indices = SI)
-        SIval[SelectPixels] <- c(SIvaltmp$SpectralIndices[[SI]])
-      }
-      r_out[[SI]] <- writeValues(r_out[[SI]], SIval, blk$row[i],format = "ENVI", overwrite = TRUE)
-    }
-    pb$tick()
-  }
-  # close files
-  r_in <- readStop(r_in)
-  if (typeof(r_inmask)=='S4'){
-    r_inmask <- readStop(r_inmask)
-  }
-  for (SI in Sel_Indices){
-    r_out[[SI]] <- writeStop(r_out[[SI]])
-    # write biophysical variable name in headers
-    HDR <- read_ENVI_header(get_HDR_name(SIpath[[SI]]))
-    HDR$`band names` <- paste('{',SI,'}',sep = '')
-    write_ENVI_header(HDR, get_HDR_name(SIpath[[SI]]))
-  }
-  return(SIpath)
-}
